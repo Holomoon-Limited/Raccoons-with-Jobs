7b354348bd05e659338b87a70f36185668a31b65
AK <altkreation@gmail.com>
2023-05-10 11:32:38 +0100

    imported textures, updated BaseCard prefab

--- Assets/Scripts/Cards/CardDisplayInformation.cs
+++ Assets/Scripts/Cards/CardDisplayInformation.cs
@@ -11,13 +11,7 @@ namespace Holo.Cards
     {
         // data from ScriptableObject
         [SerializeField] private CardScriptableObject cardData;
-        private string cardName;
-        private int power;
-        private string cardDescription;
-        private Sprite image;
-        private int id;
-        // private Effect effect;
-        
+
         // components to update with data
         [SerializeField] private TextMeshProUGUI cardNameTMP;
         [SerializeField] private TextMeshProUGUI powerTMP;
@@ -26,19 +20,11 @@ namespace Holo.Cards
         
         void Start()
         {
-            // get data from SO
-            cardName = cardData.CardName;
-            power = cardData.Power;
-            cardDescription = cardData.CardDescription;
-            image = cardData.Image;
-            id = cardData.ID;
-            // effect = cardData.Effect;
-
             // assign data to components 
-            cardNameTMP.text = cardName;
-            powerTMP.text = power.ToString();
-            cardDescriptionTMP.text = cardDescription;
-            imageImage.sprite = image;
+            cardNameTMP.text = cardData.CardName;
+            powerTMP.text = cardData.Power.ToString();
+            cardDescriptionTMP.text = cardData.CardDescription;
+            imageImage.sprite = cardData.Image;
         }
     }
 }
