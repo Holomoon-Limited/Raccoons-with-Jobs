a316763cc81c6d9ba1a0d622b73339e2e1a19016
AK <altkreation@gmail.com>
2023-05-24 16:11:03 +0100

    disabled hand view for battle scene

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using Cinemachine;
 using Holo.Input;
+using UnityEngine.SceneManagement;
 
 namespace Holo.Cam
 {
@@ -17,6 +18,8 @@ namespace Holo.Cam
         public event Action OnBoardView;
         public event Action OnHandView;
 
+        private Scene thisScene;
+
         private void OnEnable()
         {
             inputManager.OnMouseScrolledUp += DisplayBoardView;
@@ -31,6 +34,14 @@ namespace Holo.Cam
 
         void Start()
         {
+            // default to Board View in battle 
+            thisScene = SceneManager.GetActiveScene();
+            if (thisScene.name.Equals("Battle"))
+            {
+                DisplayBoardView();
+                return;
+            }
+
             playerHandCamera.Priority = 11;
             boardCamera.Priority = 10;
         }
@@ -39,12 +50,19 @@ namespace Holo.Cam
         {
             playerHandCamera.Priority = 10;
             boardCamera.Priority = 11;
+            
+            OnBoardView?.Invoke();
         }
         
         private void DisplayHandView()
         {
+            // disable hand view in Battle
+            if (thisScene.name.Equals("Battle")) return;
+
             playerHandCamera.Priority = 11;
             boardCamera.Priority = 10;
+            
+            OnHandView?.Invoke();
         }
     }
 }
